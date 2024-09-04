426cdeb1db405dc8fca084cf9cc5fe529136478b
Riz Angel Czar F. Veran IV <rveran@synergyonline.com>
2024-09-05 01:01:13 +0800

    added result component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import ElectionResults from './components/resultsElec';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          deck
-        </p>
-
+        <ElectionResults/>
       </header>
     </div>
   );
